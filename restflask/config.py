f42c3d7c4f79fcc8a2308c598153ff6b6840e6aa
Harikrishna0401 <60138659+Harikrishna0401@users.noreply.github.com>
2020-03-24 12:00:12 +0530

    Update config.py

--- restflask/config.py
+++ restflask/config.py
@@ -3,5 +3,3 @@ import os
 class DevelopmentConfig(object):
   SECRET_KEY = os.environ.get("SECRETKEY")
   
-  SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password123@localhost/mydb'
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
