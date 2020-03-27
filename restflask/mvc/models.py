5f47003531fb3e2e931e0963e6c9cf70ed390c21
Harikrishna0401 <60138659+Harikrishna0401@users.noreply.github.com>
2020-03-27 10:33:41 +0530

    Added username

--- restflask/mvc/models.py
+++ restflask/mvc/models.py
@@ -2,17 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 from mvc import app
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password123@localhost/mydb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     pid = db.Column(db.Integer,primary_key=True)
-    public_id = db.Column(db.Text, unique=True)
+    public_id = db.Column(db.Text,unique=True)
     name = db.Column(db.Text,nullable=False)
+    username = db.Column(db.Text,nullable=False)
     password = db.Column(db.Text,nullable=False)
+    email = db.Column(db.Text)
 
 class News(db.Model):
    id = db.Column(db.Integer, primary_key=True)
